6738d4faa8e587c5c1aa096a98bb8fd829f9e0cc
aleksey <ghost7676@mail.ru>
2020-03-12 09:05:38 +0400

    Test lombok

--- src/main/java/com/example/library/Application.java
+++ src/main/java/com/example/library/Application.java
@@ -2,6 +2,11 @@ package com.example.library;
 
 public class Application {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setA(4);
+        car.setB(5);
+        System.out.println(car.getA() + " " + car.getB());
+
         System.out.println();
     }
 }
